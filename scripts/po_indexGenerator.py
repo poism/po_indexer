d44373a6f32006583c40d60467532de0f9abaf2d
root <root@debian-vm.poism.com>
2012-12-29 22:29:58 -0500

    begin adding selected path dir search

--- scripts/po_indexGenerator.py
+++ scripts/po_indexGenerator.py
@@ -50,7 +50,7 @@ def writeJson(write_file,id,path,fileName,size):
 	if (id > 1):
 		write_file.write(',\n')
 
-	write_file.write('{ "f":"'+str(fileName).strip()+'" , "p":"'+str(path).strip()+'" , "s":"'+str(size).strip()+'" }')
+	write_file.write('{ "f":"'+str(fileName).strip()+'" , "p":"'+str(path).strip()+'/" , "s":"'+str(size).strip()+'" }')
 
 def writeJsonWrap(write_file,do):
 	if (do == 'start'):
